acc95411af0f3844cb835799a37c7e0febe41df7
Kevin Collins <mpowerkc@gmail.com>
2018-01-15 18:05:11 -0800

    Add POST endpoint

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,7 +5,14 @@ let routes = express.Router();
 routes
   .get('/', (req, res) => {
     res.status(200).json({message: "connected"});
-  }).get('/test', (req, res) => {
+  })
+  .get('/test', (req, res) => {
+    res.status(200).json({
+      response_type: "ephemeral",
+      text: "Hey, what up?"
+    });
+  })
+  .post('/test', (req, res) => {
     res.status(200).json({
       response_type: "ephemeral",
       text: "Hey, what up?"
