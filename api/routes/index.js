1fb7b16cd8ae3b56e35a81e1c1659777859515d2
Kevin Collins <mpowerkc@gmail.com>
2018-01-17 21:46:39 -0800

    added sample message from slack

--- api/routes/index.js
+++ api/routes/index.js
@@ -63,9 +63,37 @@ routes
 export {
   routes
 }
+
 /*
+// user event payload from slack
 {
-  "payload":
-    "{\"type\":\"interactive_message\",\"actions\":[{\"name\":\"like\",\"type\":\"button\",\"value\":\"like|resource|1\"}],\"callback_id\":\"resource-like\",\"team\":{\"id\":\"T8SM8CS9X\",\"domain\":\"kc-googlescholar\"},\"channel\":{\"id\":\"C8U3R2214\",\"name\":\"demo\"},\"user\":{\"id\":\"U8TBFRSJZ\",\"name\":\"mpowerkc\"},\"action_ts\":\"1516090144.094544\",\"message_ts\":\"1516090010.000183\",\"attachment_id\":\"1\",\"token\":\"wFQ75QDy9KLkjWXwgbxdZUNY\",\"is_app_unfurl\":false,\"response_url\":\"https:\\/\\/hooks.slack.com\\/actions\\/T8SM8CS9X\\/300439600343\\/uWe6RlFx1xBATyYeyGf55Zcv\",\"trigger_id\":\"300439600375.298722434337.9dac79a73947ad06a858ae85ce6968a8\"}"
+  "type": "interactive_message",
+  "actions": [
+    {
+      "name": "like",
+      "type": "button",
+      "value": "like|resource|2"
+    }
+  ],
+  "callback_id": "resource-like",
+  "team": {
+    "id": "T8SM8CS9X",
+    "domain": "kc-googlescholar"
+  },
+  "channel": {
+    "id": "C8U3R2214",
+    "name": "demo"
+  },
+  "user": {
+    "id": "U8TBFRSJZ",
+    "name": "mpowerkc"
+  },
+  "action_ts": "1516092460.952064",
+  "message_ts": "1516092444.000113",
+  "attachment_id": "1",
+  "token": "wFQ75QDy9KLkjWXwgbxdZUNY",
+  "is_app_unfurl": false,
+  "response_url": "https://hooks.slack.com/actions/T8SM8CS9X/300248649126/c2jIykS5JXt5hw9Bdx31MzST",
+  "trigger_id": "300453576183.298722434337.206439d63325b48c547f99f0c7aa1e29"
 }
 */
